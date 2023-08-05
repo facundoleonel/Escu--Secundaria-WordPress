83d90a8bc83f51390d201b806fd8da9bcae041b9
facundoleonel <facundopanozzo@gmail.com>
2023-08-04 22:09:44 -0300

    single

--- single.php
+++ single.php
@@ -1,11 +1,11 @@
 <?php get_header();?>
 <main>
     <?php if(have_posts()):
-        ?> <div class="container-columns is-centered is-mobile" style="border:1px solid red;"> <?php
+        ?> <div class="container-columns is-centered is-mobile" > <?php
             while(have_posts()):
                 the_post();
             ?>
-            <div class="column is-centered" style="border:1px solid blue;">
+            <div class="column is-centered" >
                 <h1><?php the_title()?></h1>
                     <div class="portada-img">
                         <?php the_post_thumbnail('large');?>
