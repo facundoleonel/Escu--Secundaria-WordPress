14327db01b3e99598d0a7e651f04d5dcb25800a6
facundoleonel <facundopanozzo@gmail.com>
2023-08-10 13:41:57 -0300

    header

--- header.php
+++ header.php
@@ -9,34 +9,34 @@
 <body>
 <header>
     <div class="container-full bg-00">
-    <div class="columns">
-    <div class="column is-4">
-        <!-- Inicio -->
-        <div class="columns is-mobile has-text-centered outline-red">
-            <div class="column is-6-mobile is-6-tablet outline-black">
-                <!-- aca va la IMAGENES DE MENU -->
-                <img src="<?= getIMG("menu-indigo.png")?>" alt="menu" class="menu">
+        <div class="columns">
+            <div class="column is-4">
+                <!-- Inicio -->
+                <div class="columns is-mobile has-text-centered outline-red">
+                    <div class="column is-6-mobile is-6-tablet outline-black">
+                        <!-- aca va la IMAGENES DE MENU -->
+                        <img src="<?= getIMG("menu-indigo.png")?>" alt="menu" class="menu">
+                    </div>
+                    <div class="column is-6 outline-black">
+                        <img class="logo" src="<?= getIMG("logo.png")?>" alt="logo"/>
+                    </div>
+                </div>
+                <!-- Fin -->
             </div>
-            <div class="column is-6 outline-black">
-                <img class="logo" src="<?= getIMG("logo.png")?>" alt="logo"/>
+            <div class="column is-8 has-text-centered ">
+                <nav class="main-nav inactive">
+                    <div class=" menu-nev breadcrumb has-succeeds-separator is-centered" aria-label="breadcrumbs">
+                        <!-- imprimimos el menu -->
+                        <?php wp_nav_menu(
+                            array(
+                                'container_class' => 'main-nav',
+                            'theme_location'=> 'menu_principal',
+                            "menu_class" => "menu-principal"));
+                            ?>
+                    </div>
+                </nav>
             </div>
         </div>
-        <!-- Fin -->
-    </div>
-    <div class="column is-8 has-text-centered ">
-        <nav class="main-nav inactive">
-            <div class=" menu-nev breadcrumb has-succeeds-separator is-centered" aria-label="breadcrumbs">
-                <!-- imprimimos el menu -->
-                <?php wp_nav_menu(
-                    array(
-                        'container_class' => 'main-nav',
-                    'theme_location'=> 'menu_principal',
-                    "menu_class" => "menu-principal"));
-                    ?>
-            </div>
-        </nav>
-    </div>
-    </div>
     </div>
 </header>
 
